c91faa033ec447853ac6054fb087aa54063b348d
eyebex <eyebex@threekings.tk>
2006-08-09 14:28:32 +0000

    Implemented parsing of the basic file structure into memory.
    git-svn-id: svn://svn.berlios.de/gale/trunk@67 2d798ef8-6e06-0410-941a-9092aa770104

--- glex/parse.php
+++ glex/parse.php
@@ -9,7 +9,7 @@ if (isset($argc)) {
     $argv=$_SERVER['argv'];
 
     if ($argc<2)
-        exit('Usage: '.basename($argv[0]).' spec=<URL to OpenGL extension specification text file>');
+        exit('Usage: '.basename($argv[0]).' spec=<URI or URL to OpenGL extension specification text file>');
 
     // Make the command line available as PHP variables.
     for ($i=0;$i<$argc;++$i) {
@@ -26,19 +26,48 @@ if (empty($spec))
 $handle=@fopen($spec,'r') or
     exit("Error: Unable to open the file \"$spec\" for reading.");
 
-// Read the all needed parts of the file into a buffer.
+// Parse the sections and contents of the file into a structure array.
+$section=NULL;
+$content=NULL;
+
 $i=0;
 while (!feof($handle)) {
-    $line=fgets($handle);
+    $line=rtrim(fgets($handle));
     ++$i;
+
     if (strlen($line)>OPENGL_SPEC_MAX_CHARS_PER_LINE)
         echo "Warning: Line $i exceeds the maximum of ".OPENGL_SPEC_MAX_CHARS_PER_LINE." characters.\n";
+
+    // We can stop parsing when we have reached e.g. the revision history.
     if (is_int(strpos($line,OPENGL_SPEC_BREAK_STRING)))
         break;
-    $buffer.=$line;
-}
 
-echo $buffer;
+    if (empty($line) || $line[0]==' ') {
+        // Switch to content parsing mode until a new section header is found.
+        if (!empty($content))
+            $content.=' ';
+        $content.=ltrim($line);
+    } else {
+        // Skip comments as shown in the OpenGL extension document template.
+        if ($line[0]=='*')
+            continue;
+
+        // Add any new section and (possibly empty) content to the structure array.
+        if (!is_null($section) && !is_null($content)) {
+            $struct[$section]=$content;
+            $section=NULL;
+            $content=NULL;
+        }
+
+        // Switch to section parsing mode until indented text in found.
+        if (!empty($section))
+            $section.=' ';
+        $section.=$line;
+    }
+}
+$struct[$section]=$content;
+
+//print_r($struct);
 
 fclose($handle);
 
