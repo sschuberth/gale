536f114f4e0c863116ff55cff7010eb8570a744a
eyebex <eyebex@threekings.tk>
2006-08-09 16:49:56 +0000

    Added basic render target class.
    git-svn-id: svn://svn.berlios.de/gale/trunk@69 2d798ef8-6e06-0410-941a-9092aa770104

---
+++ gale/include/gale/system/rendertarget.h
@@ -0,0 +1,14 @@
+#ifndef RENDERTARGET_H
+#define RENDERTARGET_H
+
+class RenderTarget {
+  public:
+    RenderTarget();
+
+  protected:
+    //void createWindow();
+
+    static unsigned int s_instances;
+};
+
+#endif // RENDERTARGET_H
---
+++ gale/source/rendertarget.cpp
@@ -0,0 +1,70 @@
+#include "gale/system/rendertarget.h"
+#include "gale/global/defines.h"
+
+#include <windows.h>
+
+unsigned int RenderTarget::s_instances=0;
+
+RenderTarget::RenderTarget() {
+    //if (s_instances==0) {
+
+    // Register a minimal class that allocates a unique device context for each window.
+    WNDCLASS cls;
+    ZeroMemory(&cls,sizeof(cls));
+    cls.style=CS_OWNDC;
+    cls.lpfnWndProc=DefWindowProc;
+    cls.lpszClassName="G";
+
+    ATOM atom=RegisterClass(&cls);
+    G_ASSERT(atom!=0)
+
+    // Create a dummy window and rendering context to access OpenGL extensions.
+    HWND wnd=CreateWindow(
+    /* lpClassName  */ MAKEINTATOM(atom),
+    /* lpWindowName */ NULL,
+    /* dwStyle      */ 0,
+    /* x            */ 0,
+    /* y            */ 0,
+    /* nWidth       */ 0,
+    /* nHeight      */ 0,
+    /* hWndParent   */ HWND_DESKTOP,
+    /* hMenu        */ NULL,
+    /* hInstance    */ NULL,
+    /* lpParam      */ this
+    );
+    G_ASSERT(wnd!=NULL)
+
+    HDC dc=GetWindowDC(wnd);
+    G_ASSERT(dc!=NULL)
+
+    // Set the device context to a pixel format that uses accelerated OpenGL.
+    PIXELFORMATDESCRIPTOR pfd;
+    ZeroMemory(&pfd,sizeof(pfd));
+    pfd.nSize=sizeof(pfd);
+    pfd.nVersion=1;
+    pfd.dwFlags=PFD_DRAW_TO_WINDOW|PFD_SUPPORT_OPENGL|PFD_GENERIC_ACCELERATED;
+
+    int format=ChoosePixelFormat(dc,&pfd);
+    G_ASSERT(format!=0)
+
+    BOOL result=SetPixelFormat(dc,format,&pfd);
+    G_ASSERT(result!=FALSE)
+
+    HGLRC rc=wglCreateContext(dc);
+    G_ASSERT(rc!=NULL)
+
+    //
+    //
+
+    result=wglDeleteContext(rc);
+    G_ASSERT(result!=FALSE)
+
+    result=ReleaseDC(wnd,dc);
+    G_ASSERT(result!=FALSE)
+
+    result=DestroyWindow(wnd);
+    G_ASSERT(result!=FALSE)
+
+    result=UnregisterClass(MAKEINTATOM(atom),NULL);
+    G_ASSERT(result!=FALSE)
+}
