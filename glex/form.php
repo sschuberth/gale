3f8d8740681af5bafb7209e68d6090c51396b540
eyebex <eyebex@threekings.tk>
2006-08-10 08:50:30 +0000

    Initial version of the web front-end form.
    git-svn-id: svn://svn.berlios.de/gale/trunk@72 2d798ef8-6e06-0410-941a-9092aa770104

---
+++ glex/form.php
@@ -0,0 +1,41 @@
+<?= '<?xml version="1.0" encoding="iso-8859-1"?>' ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<html>
+
+<head>
+    <title>glex - OpenGL Extension Specification Parser</title>
+    <style type="text/css">
+        img { border-style: none; }
+    </style>
+    <link rel="stylesheet" href="http://www.opengl.org/css/home.css" type="text/css" media="screen" />
+</head>
+
+<body>
+    <div id="right_column">
+        <div class="cap_top">Column Header</div>
+        <div class="sidecontent">
+            <hr class="sidenav"/>
+        </div>
+    </div>
+
+    <p>
+        Enter the URI or URL to OpenGL extension specification text file
+        (see <a href="http://www.opengl.org/registry/">http://www.opengl.org/registry/</a>):
+    </p>
+    <form action="">
+        <p><input type="file" size="85" accept="text/*" name="spec" /></p>
+        <p><input type="button" value="Generate code" /></p>
+    </form>
+    <p>
+        <a href="http://validator.w3.org/check?uri=referer">
+            <img src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" width="88" height="31" />
+        </a>
+        <a href="http://jigsaw.w3.org/css-validator/check/referer">
+            <img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS" width="88" height="31" />
+        </a>
+    </p>
+</body>
+
+</html>
