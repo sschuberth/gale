73eba329cf4d10bdeac220d660203bb2dfa17061
eyebex <eyebex@threekings.tk>
2006-09-24 20:27:51 +0000

    Moved CMakeLists.txt for gale to the build directory.
    git-svn-id: svn://svn.berlios.de/gale/trunk@91 2d798ef8-6e06-0410-941a-9092aa770104

--- gale/build/msvcpp71.cmd
+++ gale/build/msvcpp71.cmd
@@ -1,10 +1,10 @@
 @echo off
 
 : (Re-)create an empty output directory.
-rmdir /s /q %~dpn0 2> nul
-mkdir %~dpn0 2> nul
+rmdir /s /q "%~dpn0" 2> nul
+mkdir "%~dpn0" 2> nul
 
 : Generate the project files in the output directory.
-pushd %~dpn0
-cmake.exe -G "Visual Studio 7 .NET 2003" ..\..\source
+pushd "%~dpn0"
+cmake.exe -G "Visual Studio 7 .NET 2003" ..
 popd
