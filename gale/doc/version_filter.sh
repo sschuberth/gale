25e38ac584a513d02d5feedb5224d586dfeab692
eyebex <eyebex@threekings.tk>
2006-10-02 10:11:24 +0000

    Fixed typo.
    git-svn-id: svn://svn.berlios.de/gale/trunk@130 2d798ef8-6e06-0410-941a-9092aa770104

--- gale/doc/version_filter.sh
+++ gale/doc/version_filter.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-svn stat -v $1 | sed -nr 's/^[ A-Z?\*|!]{1,15}([0-9]+) +([0-9]+).*/Working revision \1 \/ commited revision \2/p'
+svn stat -v $1 | sed -nr 's/^[ A-Z?\*|!]{1,15}([0-9]+) +([0-9]+).*/Committed revision is \2, working revision is \1/p'
