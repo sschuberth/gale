d799d5c17daca72efe042f4b190dd76149813719
eyebex <eyebex@threekings.tk>
2006-11-23 18:56:28 +0000

    Let the build system download and parse the OpenGL registry to generate source code from the specs.
    git-svn-id: svn://svn.berlios.de/gale/trunk@219 2d798ef8-6e06-0410-941a-9092aa770104

--- gale/build/msvcpp71.cmd
+++ gale/build/msvcpp71.cmd
@@ -1,10 +1,32 @@
 @echo off
 
+: Add the Cygwin binary directory to the PATH.
+if "%1" == "" (
+    set cygwin="C:\cygwin\bin"
+) else (
+    set cygwin="%1"
+)
+
+set OLD_PATH=%PATH%
+set PATH=%cygwin%;%PATH%
+
+: Download and parse the OpenGL extension registry.
+pushd "%~dp0..\..\glex"
+bash -c "export PATH=/bin && ./registry/update_registry.sh ./registry/OpenGL.org"
+bash -c "export PATH=/bin && ./glex.sh spec=./registry/OpenGL.org/ARB/wgl_pixel_format.txt"
+mv GLEX_*.* ..\gale\include\glex
+popd
+
+: Undo any changes to variables.
+set PATH=%OLD_PATH%
+set OLD_PATH=
+set cygwin=
+
 : (Re-)create an empty output directory.
 rmdir /s /q "%~dpn0" 2> nul
 mkdir "%~dpn0" 2> nul
 
 : Generate the project files in the output directory.
 pushd "%~dpn0"
-cmake.exe -G "Visual Studio 7 .NET 2003" ..
+cmake -G "Visual Studio 7 .NET 2003" ..
 popd
