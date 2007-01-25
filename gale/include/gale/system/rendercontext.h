3dd0eb25b65f633ce7c4087557fb995227bb84ae
eyebex <eyebex@threekings.tk>
2007-01-25 01:03:26 +0000

    Added docs to finalize file.
    git-svn-id: svn://svn.berlios.de/gale/trunk@261 2d798ef8-6e06-0410-941a-9092aa770104

--- gale/include/gale/system/rendercontext.h
+++ gale/include/gale/system/rendercontext.h
@@ -1,5 +1,35 @@
+/*                                     __
+ *                      .-----..---.-.|  |.-----.
+ *                      |  _  ||  _  ||  ||  -__|
+ *                      |___  ||___._||__||_____|
+ * This file is part of |_____| the Graphics Abstraction Layer & Engine,
+ * see the project page at http://developer.berlios.de/projects/gale/
+ *
+ * Copyright (C) 2005-2007  Sebastian Schuberth <sschuberth_AT_gmail_DOT_com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
 #ifndef RENDERCONTEXT_H
 #define RENDERCONTEXT_H
+
+/**
+ * \file
+ * Render context abstraction
+ */
 
 #include "../global/platform.h"
 
@@ -7,38 +37,54 @@ namespace gale {
 
 namespace system {
 
+/**
+ * This class serves as a base for everything that requires a render context to
+ * be present, e.g. initializing OpenGL extensions.
+ */
 class RenderContext
 {
   public:
 
+    /// Creates a minimal render context.
     RenderContext();
+
+    /// Frees all resources allocated by the render context.
     ~RenderContext();
 
+    /// Returns the handle to the hidden window associated with this render
+    /// context.
     virtual HWND getWindowHandle() const {
         return m_wnd;
     }
 
+    /// Returns the handle to the device context associated with this render
+    /// context.
     virtual HDC getDeviceHandle() const {
         return m_dc;
     }
 
+    /// Returns the handle to the render context.
     virtual HGLRC getRenderHandle() const {
         return m_rc;
     }
 
   protected:
 
+    /// Destroys the render context, i.e. frees all resources except the window
+    /// class, so the method can be used by derived classes.
     void destroy();
 
+    /// Identifier for the registered window class.
     static ATOM s_atom;
 
   private:
 
+    /// Counter for the number of instances.
     static int s_instances;
 
-    HWND m_wnd;
-    HDC m_dc;
-    HGLRC m_rc;
+    HWND m_wnd; ///< Handle to the hidden window.
+    HDC m_dc;   ///< Handle to the device context.
+    HGLRC m_rc; ///< Handle to the render context.
 };
 
 } // namespace system
