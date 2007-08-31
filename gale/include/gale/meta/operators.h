3fd82039343d1ec09ef26d993a951c968c86dca7
eyebex <eyebex@threekings.tk>
2007-08-31 16:25:52 +0000

    Fixed comparison operator for equality.
    git-svn-id: svn://svn.berlios.de/gale/trunk@430 2d798ef8-6e06-0410-941a-9092aa770104

--- gale/include/gale/meta/operators.h
+++ gale/include/gale/meta/operators.h
@@ -149,7 +149,8 @@ struct OpCmpEqualEps
       T const& a,T const& b,
       T const& epsilon=std::numeric_limits<T>::epsilon())
     {
-        return math::abs(b-a)<epsilon;
+        // Note that epsilon may be 0 for integral types!
+        return math::abs(b-a)<=epsilon;
     }
 };
 
