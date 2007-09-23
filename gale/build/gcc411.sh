081b6b1f4b12577ac5812f24296fbec8993c08f0
eyebex <eyebex@threekings.tk>
2007-09-23 12:14:39 +0000

    Added GLEX downloading & parsing for Linux.
    git-svn-id: svn://svn.berlios.de/gale/trunk@452 2d798ef8-6e06-0410-941a-9092aa770104

--- gale/build/gcc411.sh
+++ gale/build/gcc411.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+pushd `dirname $0`/../glex > /dev/null
+
+# Download and parse the OpenGL extension registry if needed.
+if [ ! -e "../../glex/registry/OpenGL.org/ARB/color_buffer_float.txt" ]; then
+	echo "*** Generating local OpenGL registry ..."
+	../../glex/registry/update_registry.sh ../../glex/registry/OpenGL.org
+fi
+
+# List the extensions used in the project.
+echo "*** Parsing required OpenGL extensions ..."
+while read e; do
+	echo "Parsing file \"${e}\" ..."
+	../../glex/glex.sh spec=../../glex/registry/OpenGL.org/${e}
+done < ../build/extensions.txt
+
+popd > /dev/null
+
 pushd `dirname $0` > /dev/null
 
 buildname=`basename $0 .sh`
