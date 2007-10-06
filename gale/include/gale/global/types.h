f43bcff50c0dafb5cf59d7ebf401c499e715ed3f
eyebex <eyebex@threekings.tk>
2007-10-06 11:08:37 +0000

    Added type definitions for easier portability; adopted coding conventions.
    git-svn-id: svn://svn.berlios.de/gale/trunk@461 2d798ef8-6e06-0410-941a-9092aa770104

---
+++ gale/include/gale/global/types.h
@@ -0,0 +1,65 @@
+/*                                     __
+ *                      .-----..---.-.|  |.-----.
+ *                      |  _  ||  _  ||  ||  -__|
+ *                      |___  ||___._||__||_____|
+ * This file is part of |_____| the Graphics Abstraction Layer & Engine,
+ * see the project page at <http://developer.berlios.de/projects/gale/>.
+ *
+ * Copyright (C) 2005-2007  Sebastian Schuberth <sschuberth_AT_gmail_DOT_com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef TYPES_H
+#define TYPES_H
+
+#include "platform.h"
+
+#ifdef G_OS_LINUX
+
+typedef int8_t g_int8;
+typedef int16_t g_int16;
+typedef int32_t g_int32;
+typedef int64_t g_int64;
+
+typedef uint8_t g_uint8;
+typedef uint16_t g_uint16;
+typedef uint32_t g_uint32;
+typedef uint64_t g_uint64;
+
+typedef float g_real32;
+typedef double g_real64;
+
+#endif // G_OS_LINUX
+
+#ifdef G_OS_WINDOWS
+
+typedef INT8 g_int8;
+typedef INT16 g_int16;
+typedef INT32 g_int32;
+typedef INT64 g_int64;
+
+typedef UINT8 g_uint8;
+typedef UINT16 g_uint16;
+typedef UINT32 g_uint32;
+typedef UINT64 g_uint64;
+
+typedef float g_real32;
+typedef double g_real64;
+
+#endif // G_OS_WINDOWS
+
+#endif // TYPES_H
