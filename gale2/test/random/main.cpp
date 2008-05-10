3e0fe169c772448dc85ff61fe4f3763ac3afcdc8
eyebex <eyebex@threekings.tk>
2008-05-10 21:49:18 +0000

    Remove RandomSSE2 specific code.
    git-svn-id: svn://svn.berlios.de/gale/trunk@566 2d798ef8-6e06-0410-941a-9092aa770104

--- gale2/test/random/main.cpp
+++ gale2/test/random/main.cpp
@@ -19,43 +19,7 @@ using namespace gale::system;
 using namespace std;
 
 int main(int argc,char *argv[]) {
-    cout.setf(ios::boolalpha);
-    cout << "CPU capabilities" << endl << "----------------" << endl << endl;
-    cout << "Little-endian machine    : " << CPU.isLittleEndian() << endl << endl;
-
-    cout << "CPUID instruction        : " << CPU.hasCPUID() << endl;
-    cout << "Max. function (ext.)     : "
-         << CPU.getMaxCPUIDStdFunc() << " ("
-         << CPU.getMaxCPUIDExtFunc() << ")"
-         << endl;
-    cout << "Vendor string            : " << CPU.getVendorString() << endl << endl;
-
-    cout << "Time-Stamp Counter       : " << CPU.hasTSC() << endl;
-    cout << "Model Specific Registers : " << CPU.hasMSR() << endl;
-    cout << "CMPXCHG8 instruction     : " << CPU.hasCX8() << endl;
-    cout << "CMOV instructions        : " << CPU.hasCMOV() << endl;
-    cout << "Serial number enabled    : " << CPU.hasPSN() << endl;
-    cout << "Hyper-Threading          : " << CPU.hasHTT() << endl;
-    cout << "Enhanced SpeedStep       : " << CPU.hasEST() << endl << endl;
-
-    cout << "MMX (ext.)               : "
-         << CPU.hasMMX() << " ("
-         << CPU.hasMMXExt() << ")"
-         << endl;
-    cout << "SSE                      : " << CPU.hasSSE() << endl;
-    cout << "SSE2                     : " << CPU.hasSSE2() << endl;
-    cout << "SSE3                     : " << CPU.hasSSE3() << endl;
-    cout << "3DNow! (ext.)            : "
-         << CPU.has3DNow() << " ("
-         << CPU.has3DNowExt() << ")"
-         << endl;
-    cout << endl;
-
-#ifdef GALE_SSE2
-    RandomSSE2f random;
-#else
     RandomEcuyerf random;
-#endif
 
     Timer timer;
 
