6b4be66421246a2fc950ff5f8ce0fe43550a6b49
eyebex <eyebex@threekings.tk>
2008-07-22 20:38:07 +0000

    Set the MIME code of the generated pages to XHTML.
    git-svn-id: svn://svn.berlios.de/gale/trunk@583 2d798ef8-6e06-0410-941a-9092aa770104

--- glex/index.php
+++ glex/index.php
@@ -1,3 +1,4 @@
+<?php header("Content-Type: application/xhtml+xml"); ?>
 <?= '<?xml version="1.0" encoding="iso-8859-1"?>' ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 
