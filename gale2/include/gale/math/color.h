9de3396ae383443c4bc19f15cc943a5cef02970b
eyebex <eyebex@threekings.tk>
2008-10-05 19:03:29 +0000

    Remove type trait again due to ambiguity.
    git-svn-id: svn://svn.berlios.de/gale/trunk@678 2d798ef8-6e06-0410-941a-9092aa770104

--- gale2/include/gale/math/color.h
+++ gale2/include/gale/math/color.h
@@ -47,9 +47,6 @@ namespace math {
 template<typename T>
 struct ColorModel
 {
-    /// Definition for external access to the data type.
-    typedef T Type;
-
     /// Returns the minimum intensity value allowed for a color channel.
     static T MIN_INTENSITY() {
         return Numerics<T>::MIN();
