4e2a50fbaa35287a9c25eddb3b42e7fbdef7e576
eyebex <eyebex@threekings.tk>
2008-11-04 23:57:51 +0000

    Rework to be able to test all factory meshes with all (supported) subdivision schemes.
    git-svn-id: svn://svn.berlios.de/gale/trunk@711 2d798ef8-6e06-0410-941a-9092aa770104

--- gale2/test/mesh/main.cpp
+++ gale2/test/mesh/main.cpp
@@ -2,6 +2,8 @@
 
 #include <gale/model/mesh.h>
 
+#include <stdio.h>
+
 using namespace gale::math;
 using namespace gale::model;
 using namespace gale::wrapgl;
@@ -12,14 +14,14 @@ class TestWindow:public DefaultWindow
 
     TestWindow()
     :   DefaultWindow("test_mesh")
+    ,   m_mesh(Mesh::Factory::Tetrahedron())
+    ,   m_scheme(Mesh::Subdivider::Polyhedral)
+    ,   m_step(0)
+    ,   m_renderer(m_mesh)
     {
-        //m_mesh=Mesh::Factory::Tetrahedron();
-        //m_mesh=Mesh::Factory::Octahedron();
-        m_mesh=Mesh::Factory::Hexahedron();
+        m_camera.approach(5);
 
-        Mesh::Subdivider s(*m_mesh);
-        //s.divPolyhedral(3);
-        s.divLoop(3);
+        m_renderer.compile();
 
         glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
         glEnable(GL_CULL_FACE);
@@ -39,10 +41,137 @@ class TestWindow:public DefaultWindow
         glClear(GL_COLOR_BUFFER_BIT);
 
         m_camera.apply();
+        m_renderer.render();
+    }
 
-        Mesh::Renderer r(*m_mesh);
-        r.compile();
-        r.render();
+    void onKeyEvent(char key) {
+        static char last_mesh_key='1';
+        static char last_scheme_key='p';
+
+        if (key=='+' && m_step<5) {
+            ++m_step;
+            key=last_scheme_key;
+        }
+        else if (key=='-' && m_step>0) {
+            --m_step;
+            key=last_scheme_key;
+        }
+
+        switch (key) {
+            case 'p': {
+                last_scheme_key=key;
+                printf("Polyhedral subdivision");
+                if (last_mesh_key!='1' && last_mesh_key!='2' && last_mesh_key!='4') {
+                    printf(" not supported on this mesh");
+                }
+                else {
+                    m_scheme=Mesh::Subdivider::Polyhedral;
+                    key=last_mesh_key;
+                }
+                printf(" (step %d).\n",m_step);
+                break;
+            }
+            case 'b': {
+                last_scheme_key=key;
+                printf("Butterfly subdivision");
+                if (last_mesh_key!='1' && last_mesh_key!='2' && last_mesh_key!='4') {
+                    printf(" not supported on this mesh");
+                }
+                else {
+                    m_scheme=Mesh::Subdivider::Butterfly;
+                    key=last_mesh_key;
+                }
+                printf(" (step %d).\n",m_step);
+                break;
+            }
+            case 'l': {
+                last_scheme_key=key;
+                printf("Loop subdivision");
+                if (last_mesh_key!='1' && last_mesh_key!='2' && last_mesh_key!='4') {
+                    printf(" not supported on this mesh");
+                }
+                else {
+                    m_scheme=Mesh::Subdivider::Loop;
+                    key=last_mesh_key;
+                }
+                printf(" (step %d).\n",m_step);
+                break;
+            }
+            case 's': {
+                last_scheme_key=key;
+                printf("Sqrt3 subdivision");
+                if (last_mesh_key!='1' && last_mesh_key!='2' && last_mesh_key!='4') {
+                    printf(" not supported on this mesh");
+                }
+                else {
+                    m_scheme=Mesh::Subdivider::Sqrt3;
+                    key=last_mesh_key;
+                }
+                printf(" (step %d).\n",m_step);
+                break;
+            }
+            case 'c': {
+                last_scheme_key=key;
+                printf("Catmull-Clark subdivision");
+                if (last_mesh_key!='3') {
+                    printf(" not supported on this mesh");
+                }
+                else {
+                    m_scheme=Mesh::Subdivider::CatmullClark;
+                    key=last_mesh_key;
+                }
+                printf(" (step %d).\n",m_step);
+                break;
+            }
+            case 'd': {
+                last_scheme_key=key;
+                printf("Doo-Sabin subdivision (step %d).\n",m_step);
+                m_scheme=Mesh::Subdivider::DooSabin;
+                key=last_mesh_key;
+                break;
+            }
+        }
+
+        switch (key) {
+            case '1': {
+                delete m_mesh;
+                m_mesh=Mesh::Factory::Tetrahedron();
+                puts("Tetrahedron mesh.");
+                break;
+            }
+            case '2': {
+                delete m_mesh;
+                m_mesh=Mesh::Factory::Octahedron();
+                puts("Octahedron mesh.");
+                break;
+            }
+            case '3': {
+                delete m_mesh;
+                m_mesh=Mesh::Factory::Hexahedron();
+                puts("Hexahedron mesh.");
+                break;
+            }
+            case '4': {
+                delete m_mesh;
+                m_mesh=Mesh::Factory::Icosahedron();
+                puts("Icosahedron mesh.");
+                break;
+            }
+            case '5': {
+                delete m_mesh;
+                m_mesh=Mesh::Factory::Dodecahedron();
+                puts("Dodecahedron mesh.");
+                break;
+            }
+        }
+
+        if (key>='1' && key<='5') {
+            m_scheme(*m_mesh,m_step);
+            m_renderer.compile();
+            repaint();
+
+            last_mesh_key=key;
+        }
     }
 
     void onMouseEvent(int x,int y,int wheel,int event) {
@@ -57,10 +186,8 @@ class TestWindow:public DefaultWindow
         if (event!=ME_BUTTON_NONE) {
             if (event&ME_BUTTON_LEFT) {
                 HMat4f m=m_camera.getModelview();
-                m*=HMat4f::Factory::Translation(-m_camera.getPosition());
-                m*=HMat4f::Factory::RotationY(-mouse_diff_x*Constd::DEG_TO_RAD()*0.5);
-                m*=HMat4f::Factory::RotationX(-mouse_diff_y*Constd::DEG_TO_RAD()*0.5);
-                m*=HMat4f::Factory::Translation(m_camera.getPosition());
+                m=HMat4f::Factory::Rotation(m.getUpVector(),-mouse_diff_x*Constd::DEG_TO_RAD()*0.5)*m;
+                m=HMat4f::Factory::Rotation(m.getRightVector(),-mouse_diff_y*Constd::DEG_TO_RAD()*0.5)*m;
                 m_camera.setModelview(m);
             }
 
@@ -83,6 +210,11 @@ class TestWindow:public DefaultWindow
   private:
 
     Mesh* m_mesh;
+
+    Mesh::Subdivider::Scheme m_scheme;
+    int m_step;
+
+    Mesh::Renderer m_renderer;
 };
 
 // Enable memory leak detection, see:
