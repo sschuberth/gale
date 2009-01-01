6f32a18dea12891a0593589a81ee6d4d299573be
eyebex <eyebex@threekings.tk>
2009-01-01 22:53:54 +0000

    Add some misc. rendering helper functions.
    git-svn-id: svn://svn.berlios.de/gale/trunk@740 2d798ef8-6e06-0410-941a-9092aa770104

---
+++ gale2/include/gale/wrapgl/helpers.h
@@ -0,0 +1,48 @@
+/*                                     __
+ *                      .-----..---.-.|  |.-----.
+ *                      |  _  ||  _  ||  ||  -__|
+ *                      |___  ||___._||__||_____|
+ * This file is part of |_____| the Graphics Abstraction Layer & Engine,
+ * see the project page at <http://developer.berlios.de/projects/gale/>.
+ *
+ * Copyright (C) 2005-2008  Sebastian Schuberth <sschuberth_AT_gmail_DOT_com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef HELPERS_H
+#define HELPERS_H
+
+namespace gale {
+
+namespace wrapgl {
+
+/// Saves the current modelview and projection matrices and sets an orthogonal
+/// projection that maps each raster position exactly to a screen space pixel.
+void pushOrtho2D();
+
+/// Restores the projection and modelview matrices, which were e.g. previously
+/// saved by pushOrtho2D(), from the matrix stack.
+void popOrtho2D();
+
+/// Draws a logo into the lower left viewport corner, similar to a TV logo.
+void drawLogo();
+
+} // namespace wrapgl
+
+} // namespace gale
+
+#endif // HELPERS_H
---
+++ gale2/source/helpers.cpp
@@ -0,0 +1,135 @@
+#include "gale/model/mesh.h"
+#include "gale/wrapgl/camera.h"
+
+namespace gale {
+
+namespace wrapgl {
+
+void pushOrtho2D()
+{
+    // Save the current matrix mode.
+    GLint matrix_mode;
+    glGetIntegerv(GL_MATRIX_MODE,&matrix_mode);
+
+    // Set the modelview to identity.
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    glLoadIdentity();
+
+    // Set the projection matrix to map each raster position to a screen pixel.
+    glMatrixMode(GL_PROJECTION);
+    glPushMatrix();
+    glLoadIdentity();
+
+    GLint viewport[4];
+    glGetIntegerv(GL_VIEWPORT,viewport);
+    glOrtho(viewport[0],viewport[2],viewport[1],viewport[3],-1,1);
+
+    // Restore the matrix mode.
+    glMatrixMode(matrix_mode);
+}
+
+void popOrtho2D()
+{
+    // Save the current matrix mode.
+    GLint matrix_mode;
+    glGetIntegerv(GL_MATRIX_MODE,&matrix_mode);
+
+    // Restore the projection and modelview matrices.
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+
+    glMatrixMode(GL_MODELVIEW);
+    glPopMatrix();
+
+    // Restore the matrix mode.
+    glMatrixMode(matrix_mode);
+}
+
+void drawLogo()
+{
+    Camera* camera_orig=Camera::getAppliedCamera();
+
+    // Apply the required camera settings.
+    Camera camera_logo;
+
+    int size=math::min(camera_logo.getScreenSpace().width,camera_logo.getScreenSpace().height)/6;
+    camera_logo.setScreenSpaceDimensions(size,size);
+
+    camera_logo.setProjection(math::Mat4d::Factory::PerspectiveProjection(size,size,90*math::Constd::DEG_TO_RAD()));
+
+    float distance=3.0f;
+    math::Vec3f normal(distance,distance,distance);
+    camera_logo.setPosition(normal);
+    camera_logo.setLookTarget(math::Vec3f::ZERO());
+
+    camera_logo.apply();
+
+    // Render the logo.
+    glPushAttrib(GL_COLOR_BUFFER_BIT);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+
+    glPushAttrib(GL_ENABLE_BIT);
+    glEnable(GL_LINE_SMOOTH);
+
+    // Render the cubes.
+    model::Mesh::Renderer renderer;
+    model::Mesh* cube=model::Mesh::Factory::Hexahedron();
+
+    renderer.compile(cube);
+
+    glPushMatrix();
+    glTranslatef(1,0,0);
+    renderer.render();
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0,1,0);
+    renderer.render();
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0,0,1);
+    renderer.render();
+    glPopMatrix();
+
+    delete cube;
+
+    // Render the frame.
+    float scale_corner=1.9f,scale_length=1.5f;
+    math::Vec3f r(scale_corner,0,0),t(0,scale_corner,0),l(0,0,scale_corner),n;
+
+    glBegin(GL_LINE_LOOP);
+
+    glVertex3fv(r);
+
+    n=(~((t-r)^normal))*scale_length;
+    glVertex3fv(n+r);
+    glVertex3fv(n+t);
+
+    glVertex3fv(t);
+
+    n=(~((l-t)^normal))*scale_length;
+    glVertex3fv(n+t);
+    glVertex3fv(n+l);
+
+    glVertex3fv(l);
+
+    n=(~((r-l)^normal))*scale_length;
+    glVertex3fv(n+l);
+    glVertex3fv(n+r);
+
+    glEnd();
+
+    glPopAttrib();
+    glPopAttrib();
+
+    if (camera_orig) {
+        camera_orig->apply();
+    }
+}
+
+} // namespace wrapgl
+
+} // namespace gale
