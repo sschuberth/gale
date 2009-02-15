e5f0f861bfabc00bcd7feee0d140e01cc22f1113
eyebex <eyebex@threekings.tk>
2009-02-15 18:26:27 +0000

    Make AABB a struct; add missing copyright header.
    git-svn-id: svn://svn.berlios.de/gale/trunk@838 2d798ef8-6e06-0410-941a-9092aa770104

--- gale2/include/gale/model/boundingbox.h
+++ gale2/include/gale/model/boundingbox.h
@@ -1,3 +1,28 @@
+/*                                     __
+ *                      .-----..---.-.|  |.-----.
+ *                      |  _  ||  _  ||  ||  -__|
+ *                      |___  ||___._||__||_____|
+ * This file is part of |_____| the Graphics Abstraction Layer & Engine,
+ * see the project page at <http://developer.berlios.de/projects/gale/>.
+ *
+ * Copyright (C) 2005-2009  Sebastian Schuberth <sschuberth_AT_gmail_DOT_com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
 #ifndef BOUNDINGBOX_H
 #define BOUNDINGBOX_H
 
@@ -13,12 +38,10 @@ namespace gale {
 namespace model {
 
 /**
- * A class to represent an axis-aligned bounding box.
+ * A data structure to represent an axis-aligned bounding box.
  */
-class AABB
+struct AABB
 {
-  public:
-
     /// Creates an empty box.
     AABB()
     :   min(math::Vec3f::ZERO())
