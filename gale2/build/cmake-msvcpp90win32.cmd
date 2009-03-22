efd268ed4c91622dad150497d9ee4dd18a534a76
eyebex <eyebex@threekings.tk>
2009-03-22 19:00:10 +0000

    Delete VS 7.1 CMake wrapper script, add VS 9.0 CMake wrapper scripts instead.
    git-svn-id: svn://svn.berlios.de/gale/trunk@914 2d798ef8-6e06-0410-941a-9092aa770104

--- gale2/build/cmake-msvcpp90win32.cmd
+++ gale2/build/cmake-msvcpp90win32.cmd
@@ -3,10 +3,10 @@
 call common.cmd %1
 
 : (Re-)create an empty output directory.
-rmdir /s /q msvcpp71 2> nul
-mkdir msvcpp71 2> nul
+rmdir /s /q msvcpp90win32 2> nul
+mkdir msvcpp90win32 2> nul
 
 : Generate the project files in the output directory.
-pushd msvcpp71
-cmake -G "Visual Studio 7 .NET 2003" ..
+pushd msvcpp90win32
+cmake -G "Visual Studio 9 2008" ..
 popd
---
+++ gale2/build/cmake-msvcpp90win64.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+call common.cmd %1
+
+: (Re-)create an empty output directory.
+rmdir /s /q msvcpp90win64 2> nul
+mkdir msvcpp90win64 2> nul
+
+: Generate the project files in the output directory.
+pushd msvcpp90win64
+cmake -G "Visual Studio 9 2008 Win64" ..
+popd
