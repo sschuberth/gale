cfdd03c41192613477fa0e808952ab792c330841
eyebex <eyebex@threekings.tk>
2009-05-25 14:01:53 +0000

    Minor beautification: Add a missing colon.
    git-svn-id: svn://svn.berlios.de/gale/trunk@955 2d798ef8-6e06-0410-941a-9092aa770104

--- glex/GLEX_globals.h
+++ glex/GLEX_globals.h
@@ -1,7 +1,7 @@
 #ifndef GLEX_GLOBALS_H
 #define GLEX_GLOBALS_H
 
-// Introduced by GL_ARB_shader_objects
+// Introduced by GL_ARB_shader_objects:
 typedef char GLcharARB;
 typedef unsigned int GLhandleARB;
 
