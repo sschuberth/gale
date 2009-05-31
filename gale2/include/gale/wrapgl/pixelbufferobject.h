3788953a24fa1ae55a3f160b83bf6dcf2e699aa6
eyebex <eyebex@threekings.tk>
2009-05-31 00:53:26 +0000

    Add a wrapper for Pixel Buffer Objects.
    git-svn-id: svn://svn.berlios.de/gale/trunk@973 2d798ef8-6e06-0410-941a-9092aa770104

---
+++ gale2/include/gale/wrapgl/pixelbufferobject.h
@@ -0,0 +1,57 @@
+/*                                     __
+ *                      .-----..---.-.|  |.-----.
+ *                      |  _  ||  _  ||  ||  -__|
+ *                      |___  ||___._||__||_____|
+ * This file is part of |_____| the Graphics Abstraction Layer & Engine,
+ * see the project page at <http://developer.berlios.de/projects/gale/>.
+ *
+ * Copyright (C) 2005-2009  Sebastian Schuberth <sschuberth_AT_gmail_DOT_com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef PIXELBUFFEROBJECT_H
+#define PIXELBUFFEROBJECT_H
+
+/**
+ * \file
+ * A wrapper for Pixel Buffer Objects
+ */
+
+#include "vertexbufferobject.h"
+
+namespace gale {
+
+namespace wrapgl {
+
+#include "GLEX_ARB_pixel_buffer_object.h"
+
+/**
+ * \name Type definitions for the different buffer types, see
+ * <http://www.opengl.org/registry/specs/ARB/pixel_buffer_object.txt>
+ */
+//@{
+
+typedef VertexBufferObject<GL_PIXEL_PACK_BUFFER_ARB,GL_PIXEL_PACK_BUFFER_BINDING_ARB> PixelPackBufferObject;
+typedef VertexBufferObject<GL_PIXEL_UNPACK_BUFFER_ARB,GL_PIXEL_UNPACK_BUFFER_BINDING_ARB> PixelUnpackBufferObject;
+
+//@}
+
+} // namespace wrapgl
+
+} // namespace gale
+
+#endif // PIXELBUFFEROBJECT_H
