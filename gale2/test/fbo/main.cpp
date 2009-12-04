b7732424a960f3dee214359f1a584ad0daf178d5
eyebex <eyebex@threekings.tk>
2009-12-04 13:54:15 +0000

    Completely overhaul the FBO test to create a "feedback" effect, like a camera that is filming its own display.
    git-svn-id: svn://svn.berlios.de/gale/trunk@1096 2d798ef8-6e06-0410-941a-9092aa770104

--- gale2/test/fbo/main.cpp
+++ gale2/test/fbo/main.cpp
@@ -6,82 +6,189 @@ using namespace gale::wrapgl;
 
 class TestWindow:public DefaultWindow
 {
+    static int const TEX_SIZE    = 256;
+    static int const BORDER_SIZE =  16;
+
   public:
 
     TestWindow()
     :   DefaultWindow(_T("test_fbo"),600,600)
+    ,   m_win_camera(m_camera)
+    ,   m_read_texture(&m_fbo_textures[0])
+    ,   m_draw_texture(&m_fbo_textures[1])
     ,   m_ping_pong(0)
+    ,   m_angle1(0)
+    ,   m_angle2(0)
     {
-        m_camera.setProjection(Mat4d::Factory::OrthographicProjection(0.0,1.0,0.0,1.0));
+        unsigned char* buffer=new unsigned char[TEX_SIZE*TEX_SIZE*3];
 
-        // Set up the read and draw textures.
-        m_read_texture=&m_textures[0];
-        m_draw_texture=&m_textures[1];
-
-        unsigned char* buffer=new unsigned char[256*256*3];
-
-        // Initialize the draw texture's size (and its data to 0).
-        memset(buffer,0,256*256*3);
-
-        m_draw_texture->setData(256,256,buffer,GL_RGB,GL_UNSIGNED_BYTE,GL_RGB);
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
-
-        // Initialize the read texture.
-        for (int y=0;y<256;++y) {
-            for (int x=0;x<256;++x) {
-                if (x<0+16 || x>255-16 || y<0+16 || y>255-16) {
-                    int offset=(y*256+x)*3;
-                    buffer[offset+0]=206;
-                    buffer[offset+1]=125;
-                    buffer[offset+2]=143;
+        // Initialize the pattern texture to a frame pattern.
+        for (int y=0;y<TEX_SIZE;++y) {
+            for (int x=0;x<TEX_SIZE;++x) {
+                int offset=(y*TEX_SIZE+x)*3;
+                if (x<0+BORDER_SIZE || x>=TEX_SIZE-BORDER_SIZE || y<0+BORDER_SIZE || y>=TEX_SIZE-BORDER_SIZE) {
+                    buffer[offset+0]=255;
+                    buffer[offset+1]=255;
+                    buffer[offset+2]=255;
+                }
+                else {
+                    buffer[offset+0]=0;
+                    buffer[offset+1]=0;
+                    buffer[offset+2]=0;
                 }
             }
         }
 
-        m_read_texture->setData(256,256,buffer,GL_RGB,GL_UNSIGNED_BYTE,GL_RGB);
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+        m_pattern_texture.setData(TEX_SIZE,TEX_SIZE,buffer,GL_RGB,GL_UNSIGNED_BYTE,GL_RGB);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+
+        // Initialize the read and draw textures to 0.
+        memset(buffer,0,TEX_SIZE*TEX_SIZE*3);
+
+        m_read_texture->setData(TEX_SIZE,TEX_SIZE,buffer,GL_RGB,GL_UNSIGNED_BYTE,GL_RGB);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+
+        m_draw_texture->setData(TEX_SIZE,TEX_SIZE,buffer,GL_RGB,GL_UNSIGNED_BYTE,GL_RGB);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
 
         delete [] buffer;
 
+        // Initialize OpenGL states.
         glEnable(GL_TEXTURE_2D);
 
-        m_frame_buffer.attach(GL_COLOR_ATTACHMENT0,*m_draw_texture);
-        repaint();
-        m_draw_texture->makeCurrent();
+        // Implicitly generate texture coordinates from vertex coordinates to be
+        // able to use glRect() to draw textured screen-space aligned rectangles.
+        glTexGeni(GL_S,GL_TEXTURE_GEN_MODE,GL_OBJECT_LINEAR);
+        glEnable(GL_TEXTURE_GEN_S);
+        glTexGeni(GL_T,GL_TEXTURE_GEN_MODE,GL_OBJECT_LINEAR);
+        glEnable(GL_TEXTURE_GEN_T);
+
+        // As only screen-aligned rectangles are to be rendered, both cameras
+        // use an orthographic projection.
+        static Mat4d const p=Mat4d::Factory::OrthographicProjection(0.0,1.0,0.0,1.0,0);
+        m_fbo_camera.setProjection(p);
+        m_win_camera.setProjection(p);
+
+        // The FBO's viewport size needs to match the attached texture size.
+        m_fbo_camera.setScreenSpaceDimensions(TEX_SIZE,TEX_SIZE);
+
+        setTimeout(0.02);
     }
 
-    void onResize(int width,int height) {
-        m_camera.setScreenSpaceDimensions(width,height);
-        repaint();
+    bool onIdle() {
+        m_fbo_camera.makeCurrent();
+
+        m_frame_buffer.attach(GL_COLOR_ATTACHMENT0,*m_draw_texture);
+
+        /*
+         * Outer border frame
+         */
+
+        // Introduce some color that fades with the iterations.
+        glColor3ub(182,212,100);
+
+        // Render the current pattern.
+        glPushMatrix();
+
+        glTranslatef(0.5,0.5,0);
+        glRotatef(m_angle1,0,0,1);
+        glTranslatef(-0.5,-0.5,0);
+
+        m_pattern_texture.makeCurrent();
+        glRectf(0,0,1,1);
+
+        glPopMatrix();
+
+        /*
+         * Inner border frame
+         */
+
+        // Scaling factor to take border size into account.
+        static float const fb=static_cast<float>(TEX_SIZE-BORDER_SIZE*2)/TEX_SIZE;
+        // Cathetus length for the original border frame.
+        static float const kc=::sin(45*Constf::DEG_TO_RAD());
+
+        // Angle of the rotated border frame.
+        float ar=fmod(::abs(m_angle2-m_angle1),90)+45;
+        // Cathetus length for the rotated border frame.
+        float kr=::sin(ar*Constf::DEG_TO_RAD());
+        // Scaling factor to take the rotation into account.
+        float fr=kc/kr;
+
+        // Scaling factor tat takes both the border size and rotation into account.
+        float f=fr*fb;
+
+        // Render the previous frame.
+        glPushMatrix();
+
+        glTranslatef(0.5,0.5,0);
+        glRotatef(m_angle2,0,0,1);
+        glScalef(f,f,1);
+        glTranslatef(-0.5,-0.5,0);
+
+        m_read_texture->makeCurrent();
+        glRectf(0,0,1,1);
+
+        glPopMatrix();
+
+        // Swap read & draw textures.
+        m_draw_texture=&m_fbo_textures[m_ping_pong];
+        m_ping_pong=(m_ping_pong+1)&1;
+        m_read_texture=&m_fbo_textures[m_ping_pong];
+
+        return true;
+    }
+
+    void onTimeout() {
+        static int counter1=0,counter2=10;
+
+        if (counter1++>=360) {
+            counter1-=360;
+        }
+        if (counter2++>=360) {
+            counter2-=360;
+        }
+
+        float step1=::sin(counter1*Constf::DEG_TO_RAD())*4;
+        float step2=::sin(counter2*Constf::DEG_TO_RAD())*4;
+
+        m_angle1+=step1;
+        m_angle2+=step2;
     }
 
     void onRender() {
-        m_camera.makeCurrent();
+        // Render a screen-space aligned quad to the window's viewport with the
+        // current draw texture applied.
+        m_win_camera.makeCurrent();
 
-        // Render a screen-space aligned quad with the read texture applied.
-        glBegin(GL_QUADS);
-            glTexCoord2f(0.0f,0.0f);
-            glVertex2f(0.0f,0.0f);
+        m_frame_buffer.setCurrent(0);
+        m_draw_texture->makeCurrent();
 
-            glTexCoord2f(1.0f,0.0f);
-            glVertex2f(1.0f,0.0f);
+        glRectf(0,0,1,1);
+    }
 
-            glTexCoord2f(1.0f,1.0f);
-            glVertex2f(1.0f,1.0f);
-
-            glTexCoord2f(0.0f,1.0f);
-            glVertex2f(0.0f,1.0f);
-        glEnd();
+    void onResize(int width,int height) {
+        // Just adjust the viewport, not the projection.
+        m_win_camera.setScreenSpaceDimensions(width,height);
+        repaint();
     }
 
   private:
 
-    int m_ping_pong;
+    Camera m_fbo_camera;
+    Camera& m_win_camera;
 
-    Texture2D m_textures[2];
+    Texture2D m_pattern_texture,m_fbo_textures[2];
     Texture2D *m_read_texture,*m_draw_texture;
 
     FrameBufferObject m_frame_buffer;
+
+    int m_ping_pong;
+
+    float m_angle1,m_angle2;
 };
 
 // Enable memory leak detection, see:
