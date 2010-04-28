ad36fd3f379186a398bd428c075a0350341e3739
eyebex <eyebex@threekings.tk>
2010-04-28 14:23:43 +0000

    Make the script work when called from a different directory.
    git-svn-id: svn://svn.berlios.de/gale/trunk@1160 2d798ef8-6e06-0410-941a-9092aa770104

--- glex/registry/update_all.sh
+++ glex/registry/update_all.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-find -maxdepth 1 -type d -regex "\./[^\.].+" -exec update_registry.sh {} \;
+pushd `dirname $0` > /dev/null
+
+find -maxdepth 1 -type d -regex "\./[^\.].+" -exec ./update_registry.sh {} \;
+
+popd > /dev/null
