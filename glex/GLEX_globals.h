6dcb08f4e530b08f9dc669c9f44e310bfa56bbeb
eyebex <eyebex@threekings.tk>
2010-04-28 10:45:22 +0000

    Only define GL_TEXTURE_BINDING_3D_EXT if it is not already defined to avoid warnings.
    git-svn-id: svn://svn.berlios.de/gale/trunk@1155 2d798ef8-6e06-0410-941a-9092aa770104

--- glex/GLEX_globals.h
+++ glex/GLEX_globals.h
@@ -2,7 +2,9 @@
 #define GLEX_GLOBALS_H
 
 // Missing from GL_EXT_texture3D:
-#define GL_TEXTURE_BINDING_3D_EXT 0x806A
+#ifndef GL_TEXTURE_BINDING_3D_EXT
+    #define GL_TEXTURE_BINDING_3D_EXT 0x806A
+#endif
 
 // Introduced by GL_ARB_shader_objects:
 typedef char GLcharARB;
