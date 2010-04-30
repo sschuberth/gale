443eb77dcd6beb7fa3855863b0d7d8594e04df42
eyebex <eyebex@threekings.tk>
2010-04-30 14:29:15 +0000

    Move the parsers to their own file.
    git-svn-id: svn://svn.berlios.de/gale/trunk@1169 2d798ef8-6e06-0410-941a-9092aa770104

---
+++ glex2/parse.php
@@ -0,0 +1,166 @@
+<?php
+
+function parseFile($parser,$file,&$table) {
+    // Parse the file into an associative array.
+    $handle=@fopen($file,'r') or exit('Error: Unable to open the file "'.$file.'" for reading.');
+
+    while (!feof($handle)) {
+        $line=fgets($handle);
+
+        // Strip comments.
+        $pos=strpos($line,'#');
+        if ($pos!==FALSE) {
+            $line=substr($line,0,$pos);
+        }
+
+        $line=preg_replace('/\/\*.*\*\//','',$line);
+
+        // Remove leading and trailing whitespaces.
+        $line=trim($line);
+
+        // If the line is empty now, there is nothing to do.
+        if (empty($line)) {
+            continue;
+        }
+
+        call_user_func_array($parser,array($line,&$table));
+    }
+
+    fclose($handle);
+}
+
+function callbackEnumSpec($line,&$table) {
+    // Example:
+    // passthru: /* AttribMask */
+    if (strpos($line,'passthru:')===0) {
+        return;
+    }
+
+    // Example:
+    // VERSION_1_1 enum:
+    if (preg_match('/(.+) enum:$/',$line,$matches)) {
+        $table[$matches[1]]=array();
+        end($table);
+    }
+    // Example:
+    // 	DEPTH_BUFFER_BIT				= 0x00000100	# AttribMask
+    else if (preg_match('/(\w+)\s*=\s*(\w+)$/',$line,$matches)) {
+        $table[key($table)]['GL_'.$matches[1]]=$matches[2];
+    }
+    // Example:
+    // 	use ARB_depth_buffer_float	    DEPTH_COMPONENT32F
+    else if (preg_match('/use\s+(\w+)\s+(\w+)$/',$line,$matches)) {
+        $table[key($table)]['GL_'.$matches[2]]=$matches[1];
+    }
+}
+
+function callbackTypeMap($line,&$table) {
+    // Example:
+    // AccumOp,*,*,			    GLenum,*,*
+    $line=str_replace(array(" ","\t"),array("",""),$line);
+    $entry=explode(',',$line);
+    $table[$entry[0]]=$entry[3];
+}
+
+function parseEnumSpec($file,&$table) {
+    global $debug;
+
+    if ($debug>=1) {
+        echo "*** DEBUG parseEnumSpec() *** Opening \"$file\" for reading.\n";
+    }
+
+    parseFile('callbackEnumSpec',$file,$table);
+    reset($table);
+
+    // Resolve "use" directives.
+    function resolveUseDirectives(&$value,$key,$table) {
+        global $debug;
+
+        if (is_array($value)) {
+            return;
+        }
+
+        // If $value is an extension name, we get its defines, else return.
+        $defines=$table[$value];
+        if (empty($defines)) {
+            return;
+        }
+
+        // We are done if we find a value.
+        $value=$defines[$key];
+        if (!empty($value)) {
+            return;
+        }
+
+        if ($debug>=1) {
+            echo "*** DEBUG resolveUseDirectives() *** \"$key\" not found.\n";
+        }
+
+        // Try some heuristics for $key.
+        $vendors=array(
+            '3DFX','3DL'
+        ,   'AMD','APPLE','ARB','ATI'
+        ,   'EXT'
+        ,   'GREMEDY'
+        ,   'HP'
+        ,   'I3D','IBM','INGR','INTEL'
+        ,   'MESA','MESAX'
+        ,   'NV'
+        ,   'OES','OML'
+        ,   'PGI'
+        ,   'REND'
+        ,   'S3','SGI','SGIS','SGIX','SUN','SUNX'
+        ,   'WIN'
+        );
+
+        $pos=strrpos($key,'_');
+        $suffix=substr($key,$pos+1);
+        if (in_array($suffix,$vendors)) {
+            $key2=substr($key,0,$pos);
+        }
+
+        $value=$defines[$key2];
+        if (!empty($value)) {
+            return;
+        }
+
+        // In case the extension name is wrong, search the whole table.
+        foreach ($table as $defines) {
+            $value=$defines[$key];
+            if (is_numeric($value)) {
+                return;
+            }
+
+            $value=$defines[$key2];
+            if (is_numeric($value)) {
+                return;
+            }
+        }
+    }
+
+    array_walk_recursive($table,'resolveUseDirectives',$table);
+
+    if ($debug>=2) {
+        echo "*** DEBUG parseEnumSpec() *** BEGIN\n";
+        var_dump($table);
+        echo "*** DEBUG parseEnumSpec() *** END\n";
+    }
+}
+
+function parseTypeMap($file,&$table) {
+    global $debug;
+
+    if ($debug>=1) {
+        echo "*** DEBUG parseTypeMap() *** Opening \"$file\" for reading.\n";
+    }
+
+    parseFile('callbackTypeMap',$file,$table);
+
+    if ($debug>=2) {
+        echo "*** DEBUG parseTypeMap() *** BEGIN\n";
+        var_dump($table);
+        echo "*** DEBUG parseTypeMap() *** END\n";
+    }
+}
+
+?>
