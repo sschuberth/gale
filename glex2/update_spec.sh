b512ad1ecc4c49987c55a1f26375a246b7995932
eyebex <eyebex@threekings.tk>
2010-05-04 15:10:28 +0000

    Always create the "spec" directory below the script directory, and also download the WGL specs.
    git-svn-id: svn://svn.berlios.de/gale/trunk@1198 2d798ef8-6e06-0410-941a-9092aa770104

--- glex2/update_spec.sh
+++ glex2/update_spec.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+pushd `dirname $0` > /dev/null
+
 wget --timestamping --directory-prefix=spec \
     http://www.opengl.org/registry/api/enumext.spec \
     http://www.opengl.org/registry/api/gl.spec \
-    http://www.opengl.org/registry/api/gl.tm
+    http://www.opengl.org/registry/api/gl.tm \
+    http://www.opengl.org/registry/api/wglenumext.spec \
+    http://www.opengl.org/registry/api/wgl.spec \
+    http://www.opengl.org/registry/api/wgl.tm
+
+popd > /dev/null
