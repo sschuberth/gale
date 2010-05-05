125a5aa2a2c9541b5dafcfd3febd3087c0a22b84
eyebex <eyebex@threekings.tk>
2010-05-05 16:30:07 +0000

    Adjust the web page layout to the input required for glex 2.
    git-svn-id: svn://svn.berlios.de/gale/trunk@1221 2d798ef8-6e06-0410-941a-9092aa770104

--- glex2/index.php
+++ glex2/index.php
@@ -7,16 +7,51 @@
 <head>
     <title>glex - An OpenGL Extension Specification Parser (Version 2)</title>
     <link type="text/css" href="style.css" rel="stylesheet" />
-    <script type="text/javascript">
-        function openWindow(URL) {
-            if (URL.length>0) {
-                window.open(URL,'spec').focus();
-            }
-        }
-    </script>
 </head>
 
-<?php require_once 'functions.php'; ?>
+<?php
+
+function drawTableBorder($prefix,$color,$edge=FALSE,$colspan=1) {
+    $color_url=urlencode($color);
+
+    echo '<tr>';
+    echo '<td class="corner"><img src="images/corner-'.$prefix.'l-'.$color_url.'.png" alt="" /></td>';
+
+    echo '<td colspan="'.$colspan.'" style="width: 100%; background-color: '.$color;
+    if ($edge) {
+        echo '; background-image: url(images/edge-'.$prefix.'.png); background-repeat: repeat-x';
+    }
+    echo '"></td>';
+
+    echo '<td class="corner"><img src="images/corner-'.$prefix.'r-'.$color_url.'.png" alt="" /></td>';
+    echo '</tr>';
+}
+
+function showSourceCode($file) {
+    global $geshi;
+
+    $f=basename($file);
+    echo '
+    <tr>
+        <td><!-- Corner spacer --></td>
+        <td><a href="download.php?file='.$f.'">'.$f.'</a>&nbsp;(<a href="download.php?file='.$f.'&amp;unix2dos=1">DOS&nbsp;format</a>)</td>
+        <td style="width: 100%; padding-left: 10px"><hr /></td>
+        <td><!-- Corner spacer --></td>
+    </tr>
+    ';
+
+    echo '<tr>';
+    echo '<td><!-- Corner spacer --></td>';
+
+    $geshi->load_from_file($file);
+    $geshi->set_language("cpp");
+    echo '<td colspan="2">'.$geshi->parse_code().'</td>';
+
+    echo '<td><!-- Corner spacer --></td>';
+    echo '</tr>';
+}
+
+?>
 
 <body>
     <table style="width: 50%">
@@ -39,7 +74,7 @@
 include_once 'geshi/geshi.php';
 
 if (empty($error)) {
-    if (empty($spec)) {
+    if (empty($es) || empty($fs) || empty($tm)) {
         echo '<div style="height: 128px"></div>';
     }
     else {
@@ -52,29 +87,29 @@ if (empty($error)) {
 
         echo '<div style="height: 32px"></div>';
 
-        // writeMacroHeader
-        if (!empty($p)) {
+        // writeFunctionHeaderFile
+        if (!empty($f)) {
             echo '<table style="width: 70%">';
                 drawTableBorder('t','white',FALSE,2);
-                showSourceCode($p);
+                showSourceCode($f);
                 drawTableBorder('b','white',FALSE,2);
             echo '</table>';
 
             echo '<div style="height: 16px"></div>';
         }
 
-        // writePrototypeHeader
-        if (!empty($h)) {
+        // writeEnumHeaderFile
+        if (!empty($e)) {
             echo '<table style="width: 70%">';
                 drawTableBorder('t','white',FALSE,2);
-                showSourceCode($h);
+                showSourceCode($e);
                 drawTableBorder('b','white',FALSE,2);
             echo '</table>';
 
             echo '<div style="height: 16px"></div>';
         }
 
-        // writeInitializationCode
+        // writeInitializationCodeFile
         if (!empty($c)) {
             echo '<table style="width: 70%">';
                 drawTableBorder('t','white',FALSE,2);
@@ -119,39 +154,53 @@ else {
 ?>
 
     <form action="glex.php">
-        <table style="width: 70%">
+        <table style="width: 50%">
             <?php drawTableBorder('t','#c7d1d2',TRUE); ?>
             <tr>
                 <td style="background-color: #d3e0e7"><!-- Corner spacer --></td>
                 <td style="background-color: #d3e0e7; padding: 10px">
+                    <?php $url='http://www.opengl.org/registry/api/enumext.spec'; ?>
                     Enter the URL to an OpenGL extension enumerants specification file
-                    (e.g. <a href="http://www.opengl.org/registry/api/enumext.spec">http://www.opengl.org/registry/api/enumext.spec</a>):
-                    <input type="text" size="100%" name="es" value="<?= $es ?>" />
+                    (e.g. <a href="<?= $url ?>"><?= $url ?></a>):
+                    <p><input type="text" size="100%" name="es" value="<?= empty($es)?$url:$es ?>" /></p>
                 </td>
                 <td style="background-color: #d3e0e7"><!-- Corner spacer --></td>
             </tr>
             <tr>
                 <td style="background-color: #d3e0e7"><!-- Corner spacer --></td>
                 <td style="background-color: #d3e0e7; padding: 10px">
+                    <?php $url='http://www.opengl.org/registry/api/gl.spec'; ?>
                     Enter the URL to an OpenGL extension functions specification file
-                    (e.g. <a href="http://www.opengl.org/registry/api/gl.spec">http://www.opengl.org/registry/api/gl.spec</a>):
-                    <input type="text" size="100%" name="fs" value="<?= $fs ?>" />
+                    (e.g. <a href="<?= $url ?>"><?= $url ?></a>):
+                    <p><input type="text" size="100%" name="fs" value="<?= empty($fs)?$url:$fs ?>" /></p>
                 </td>
                 <td style="background-color: #d3e0e7"><!-- Corner spacer --></td>
             </tr>
             <tr>
                 <td style="background-color: #d3e0e7"><!-- Corner spacer --></td>
                 <td style="background-color: #d3e0e7; padding: 10px">
+                    <?php $url='http://www.opengl.org/registry/api/gl.tm'; ?>
                     Enter the URL to an OpenGL typemap file
-                    (e.g. <a href="http://www.opengl.org/registry/api/gl.tm">http://www.opengl.org/registry/api/gl.tm</a>):
-                    <input type="text" size="100%" name="tm" value="<?= $tm ?>" />
+                    (e.g. <a href="<?= $url ?>"><?= $url ?></a>):
+                    <p><input type="text" size="100%" name="tm" value="<?= empty($tm)?$url:$tm ?>" /></p>
                 </td>
                 <td style="background-color: #d3e0e7"><!-- Corner spacer --></td>
             </tr>
             <tr>
-                <td style="background-color: #d3e0e7" colspan="3">
+                <td style="background-color: #d3e0e7"><!-- Corner spacer --></td>
+                <td style="background-color: #d3e0e7; padding: 10px">
+                    Enter the name of an API to generate code for
+                    (e.g. "ARB_framebuffer_object" or "VERSION_2_0", optional):
+                    <p><input type="text" size="100%" name="api" value="<?= empty($api)?'VERSION_2_0':$api ?>" /></p>
+                </td>
+                <td style="background-color: #d3e0e7"><!-- Corner spacer --></td>
+            </tr>
+            <tr>
+                <td style="background-color: #d3e0e7"><!-- Corner spacer --></td>
+                <td style="background-color: #d3e0e7; padding: 10px; text-align: center">
                     <input type="submit" value="Generate code" />
                 </td>
+                <td style="background-color: #d3e0e7"><!-- Corner spacer --></td>
             </tr>
             <?php drawTableBorder('b','#678285',TRUE); ?>
         </table>
@@ -163,9 +212,8 @@ else {
         <?php drawTableBorder('t','white',FALSE,5); ?>
         <tr>
             <td colspan="7" style="text-align: center; font-size: 8pt">
-            <a href="http://gale.berlios.de/glex2/">glex</a> is part of the <a href="http://developer.berlios.de/projects/gale/">gale project</a> generously hosted by <a href="http://www.berlios.de/">BerliOS</a><br />
-                Source code high-lighting is performed by <a href="http://qbnz.com/highlighter/">GeSHi</a> <?= GESHI_VERSION ?><br />
-                &nbsp;
+                <a href="http://gale.berlios.de/glex2/">glex</a> is part of the <a href="http://developer.berlios.de/projects/gale/">gale project</a> generously hosted by <a href="http://www.berlios.de/">BerliOS</a>
+                <br />&nbsp;
             </td>
         </tr>
         <tr>
@@ -188,7 +236,7 @@ else {
         <tr>
             <td colspan="7" style="text-align: center; font-size: 8pt">
                 &nbsp;<br />
-                <?= getFunctionsRevision(); ?> &bull; <?= getFunctionsDate(); ?>
+                Source code high-lighting is performed by <a href="http://qbnz.com/highlighter/">GeSHi</a> <?= GESHI_VERSION ?><br />
             </td>
         </tr>
         <?php drawTableBorder('b','white',FALSE,5); ?>
