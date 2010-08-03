27fd9974d93e7dae649b47b4c04b84ddd4f7adf4
eyebex <eyebex@threekings.tk>
2010-08-03 17:33:49 +0000

    Add support for cURL as a fallback for Wget.
    git-svn-id: svn://svn.berlios.de/gale/trunk@1372 2d798ef8-6e06-0410-941a-9092aa770104

--- glex2/update_spec.sh
+++ glex2/update_spec.sh
@@ -2,12 +2,24 @@
 
 pushd `dirname $0` > /dev/null
 
-wget --timestamping --directory-prefix=spec \
-    http://www.opengl.org/registry/api/enumext.spec \
-    http://www.opengl.org/registry/api/gl.spec \
-    http://www.opengl.org/registry/api/gl.tm \
-    http://www.opengl.org/registry/api/wglenumext.spec \
-    http://www.opengl.org/registry/api/wglext.spec \
+urls="
+    http://www.opengl.org/registry/api/enumext.spec
+    http://www.opengl.org/registry/api/gl.spec
+    http://www.opengl.org/registry/api/gl.tm
+    http://www.opengl.org/registry/api/wglenumext.spec
+    http://www.opengl.org/registry/api/wglext.spec
     http://www.opengl.org/registry/api/wgl.tm
+"
+
+if [ -n "$(wget --version 2> /dev/null)" ]; then
+    wget --timestamping --directory-prefix=spec $urls
+elif [ -n "$(curl --version 2> /dev/null)" ]; then
+    mkdir spec 2> /dev/null
+    cd spec
+    for i in $urls; do
+        echo Updating $i
+        curl --remote-name --remote-time --time-cond $(basename $i) $i
+    done
+fi
 
 popd > /dev/null
