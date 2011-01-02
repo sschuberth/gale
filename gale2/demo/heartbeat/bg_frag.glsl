8c29da626f1770b3d96d0f85ca7a5f1c22d8ebd0
eyebex <eyebex@threekings.tk>
2011-01-02 13:43:52 +0100

    gale2: Fix GLSL cast warnings

--- gale2/demo/heartbeat/bg_frag.glsl
+++ gale2/demo/heartbeat/bg_frag.glsl
@@ -6,13 +6,13 @@ uniform vec4 color1;
 void main()
 {
     // Calculate the adjacent and opposite cathetus lengths.
-    vec2 cath=vec2(gl_FragCoord.x,gl_FragCoord.y)-viewport*0.5;
+    vec2 cath=gl_FragCoord.xy-vec2(viewport)*0.5;
 
     // Calculate the angle in range [0,360[.
     float angle=degrees(atan(cath.y,cath.x))+180.0;
 
     // Calculate the section we are in.
-    float section=floor(angle/360.0*sections);
+    float section=floor(angle/360.0*float(sections));
 
     gl_FragColor=mix(color0,color1,mod(section,2.0));
 }
