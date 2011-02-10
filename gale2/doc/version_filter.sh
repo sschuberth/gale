f3dd5a6918a44652de36287e3bf144661ba7a645
eyebex <eyebex@threekings.tk>
2011-02-10 11:49:34 +0100

    gale2: Adapt sed usage to that used in other scripts

--- gale2/doc/version_filter.sh
+++ gale2/doc/version_filter.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git rev-list --max-count=1 HEAD -- $1 | sed -nr 's/^([0-9a-f]{7}).+/Last modified in \1/p'
+git rev-list --max-count=1 HEAD -- $1 | sed -rn "s/^([0-9a-f]{7}).+/Last modified in \1/p"
