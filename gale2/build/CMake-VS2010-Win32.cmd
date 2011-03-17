b886ee3ae609c88b27dc17a5515c0c50d5506f9d
eyebex <eyebex@threekings.tk>
2011-03-17 13:49:46 +0100

    gale2: Add VS2010 scripts
    VS2010 seems to create slightly smaller code than VS2008 if used with Crinkler,
    but still noticeably larger code than VS2005.

---
+++ gale2/build/CMake-VS2010-Win32.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+call opengl-parse.cmd
+
+: (Re-)create an empty output directory.
+rmdir /s /q VS2010-Win32 2> nul
+mkdir VS2010-Win32 2> nul
+
+: Generate the project files in the output directory.
+pushd VS2010-Win32
+cmake -G "Visual Studio 10" ..
+if errorlevel 1 pause
+popd
