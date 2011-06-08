74c6c0edf3b43cce70be9c192df3cb898bf798c6
eyebex <eyebex@threekings.tk>
2011-06-08 19:32:53 +0200

    gale2: Remove some superfluous "Done" status lines

--- gale2/build/opengl-parse.cmd
+++ gale2/build/opengl-parse.cmd
@@ -28,13 +28,11 @@ del GLEX_*.*
 : Download the OpenGL specification files if needed.
 echo *** Updating OpenGL specification files ...
 bash -c "export PATH=$PATH:/bin && ../../glex2/update_spec.sh"
-echo *** Done
 
 : Read the OpenGL APIs used in the project.
 echo *** Parsing required OpenGL APIs ...
 bash -c "export PATH=$PATH:/bin && ../../glex2/glex.sh es=../../glex2/spec/enumext.spec fs=../../glex2/spec/gl.spec tm=../../glex2/spec/gl.tm api=@../build/opengl-apis.txt"
 bash -c "export PATH=$PATH:/bin && ../../glex2/glex.sh es=../../glex2/spec/wglenumext.spec fs=../../glex2/spec/wglext.spec tm=../../glex2/spec/wgl.tm api=@../build/opengl-apis.txt"
-echo *** Done
 
 if exist *.stackdump pause
 
